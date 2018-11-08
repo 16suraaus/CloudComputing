850fc52381b68070f9cba9e7d2a94b317eea9e1b
Austin Nicholas Sura <asura@student04.cse.nd.edu>
2018-11-08 02:40:30 -0500

    Assignment A1

---
+++ cloudA1/Versions361999.js
@@ -0,0 +1,2 @@
+DownloadDateTime="2018-09-14 19:21:44";
+Outcome[0]="Downloaded";ObjectName[0]="IntegrateLight";ContributorTag[0]="Cousin Ricky";VersionNumber[0]="1.0";SubmissionDate[0]="2016-11-14 21:00:18";
